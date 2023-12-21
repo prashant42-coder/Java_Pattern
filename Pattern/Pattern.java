7fd05082e17d8bad7b714eef7c7161fe0b79a000
prashant42-coder <122604273+prashant42-coder@users.noreply.github.com>
2023-12-21 18:13:33 +0530

    Add files via upload
    java pattern Codes -V1.0

---
+++ Pattern/Pattern.java
@@ -0,0 +1,34 @@
+import java.util.*;
+public class Pattern
+{
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.print("Enter the stars in each side of square: ");
+            int sideSize = sc.nextInt();
+
+            for (int i = 0; i < sideSize; i++ )
+            {
+                for (int j = 0 ; j < sideSize; j++ )
+                {
+                    if (i == 0 || i == sideSize - 1 || j == 0 || j == sideSize - 1)
+                    {
+                        System.out.print("*"+" ");
+                    }
+                    else {
+                        System.out.print(" "+ " ");
+                    }
+                }
+                System.out.println();
+            }
+        }
+    }
+}
+
+//output:-
+//Enter the stars in each side of square:
+//        5
+//        * * * * *
+//        *       *
+//        *       *
+//        *       *
+//        * * * * * 
